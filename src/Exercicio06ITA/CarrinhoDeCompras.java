dbd49b6fdb170da9ce34bd127ddb41c4ba4faeb2
MateusBCC020 <mateusdossantosribeiro100@usp.br>
2021-03-08 04:32:57 -0300

    Ecercicio 6 ITA

---
+++ src/Exercicio06ITA/CarrinhoDeCompras.java
@@ -0,0 +1,21 @@
+package Exercicio06ITA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+	List<Pizza> pizzasNoCarrinho = new ArrayList<>();
+	
+	public void adicionarPizza(Pizza pizza) {
+		if(pizza.listaIngredientes.size() <= 0) return;
+		pizzasNoCarrinho.add(pizza);
+	}
+	
+	public double retornaValorTotal() {
+		double valorTotal = 0;
+		for(Pizza elemento : pizzasNoCarrinho) {
+			valorTotal += elemento.getPreco();
+		}
+		return valorTotal;
+	}
+}
